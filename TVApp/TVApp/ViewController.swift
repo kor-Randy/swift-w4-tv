090b77dc89614a10bcc88fdbe22f31e17637359d
Tree.JI <tree.ji@kakaocorp.com>
2021-01-25 13:40:59 +0900

    add: 검색 바

--- TVApp/TVApp/ViewController.swift
+++ TVApp/TVApp/ViewController.swift
@@ -8,6 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.backgroundImage = UIImage()
+        searchBar.placeholder = "검색어를 입력해주세요"
+        searchBar.clipsToBounds = true
+        return searchBar
+    }()
+    
+    private var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 20
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.backgroundColor = .white
+        return cv
+    }()
     
     private var starBarButton: UIBarButtonItem = {
         let button = UIBarButtonItem()
@@ -19,19 +36,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         initNavigationBar()
-        self.view.backgroundColor = .white
-        
+        setSearchBar()
+        view.backgroundColor = .white
     }
     
-    private func initNavigationBar(){
-        self.navigationItem.title = "kakaoTV"
+    private func initNavigationBar() {
+        navigationItem.title = "kakaoTV"
         starBarButton.target = self
-        starBarButton.action = #selector(self.tappedStar)
+        starBarButton.action = #selector(tappedStar)
         navigationItem.rightBarButtonItem = starBarButton
-        
     }
     
-    @objc func tappedStar(){
-        //TODO : 즐겨찾기 화면 전환
+    private func setSearchBar() {
+        view.addSubview(searchBar)
+        searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 2).isActive = true
+        searchBar.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        searchBar.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9).isActive = true
+    }
+    
+    @objc func tappedStar() {
+        // TODO: 즐겨찾기 화면 전환
     }
 }
