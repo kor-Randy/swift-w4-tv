4dafc9894e2b5ee41b2cbcf75331577a8f07c754
Tree.JI <tree.ji@kakaocorp.com>
2021-01-25 13:24:55 +0900

    add: 즐겨찾기 바 버튼

--- TVApp/TVApp/ViewController.swift
+++ TVApp/TVApp/ViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var starBarButton: UIBarButtonItem = {
+        let button = UIBarButtonItem()
+        button.image = UIImage(systemName: "heart.fill")
+        button.style = .plain
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         initNavigationBar()
@@ -17,5 +25,13 @@ class ViewController: UIViewController {
     
     private func initNavigationBar(){
         self.navigationItem.title = "kakaoTV"
+        starBarButton.target = self
+        starBarButton.action = #selector(self.tappedStar)
+        navigationItem.rightBarButtonItem = starBarButton
+        
+    }
+    
+    @objc func tappedStar(){
+        //TODO : 즐겨찾기 화면 전환
     }
 }
