26a2970b5e49fcb8ba45abf232ae64cb17bb2df7
Tree.JI <tree.ji@kakaocorp.com>
2021-01-25 10:49:57 +0900

    init: 프로젝트 생성

---
+++ TVApp/TVApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TVApp
+//
+//  Created by 지현우 on 2021/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
