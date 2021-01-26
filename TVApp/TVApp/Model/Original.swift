38fb9b1d535ab2a5b69d36bc74c2713fd2685581
Tree.JI <tree.ji@kakaocorp.com>
2021-01-26 13:42:50 +0900

    feature: json 파싱, 뷰모델 생성

---
+++ TVApp/TVApp/Model/Original.swift
@@ -0,0 +1,23 @@
+//
+//  Original.swift
+//  TVApp
+//
+//  Created by 지현우 on 2021/01/26.
+//
+
+import Foundation
+
+// MARK: - Original
+
+struct Original: Decodable {
+    var clip: Clip
+    var channel: Channel
+}
+
+// MARK: - Clip
+
+struct Clip: Codable {
+    var thumbnailUrl: String
+    var duration: Int
+    var title: String
+}
